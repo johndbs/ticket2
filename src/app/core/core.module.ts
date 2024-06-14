10b8342ef50e0c2dd367428cbe02ec57a75dd76d
johndbs <83272366+johndbs@users.noreply.github.com>
2024-06-14 12:14:09 +0200

    feat: Add home page and navigation component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { UiModule } from '../ui/ui.module';
+import { NavComponent } from './components/nav/nav.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavComponent
+  ],
   imports: [
     CommonModule,
     RouterModule
   ],
   exports: [
-    UiModule
+    UiModule,
+    NavComponent
   ]
 })
 export class CoreModule { }
