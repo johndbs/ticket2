a9460cdb7f86dcf492a2582065745224c3f6f384
johndbs <83272366+johndbs@users.noreply.github.com>
2024-06-17 17:01:56 +0200

    feat: SharedModule: - export ReactiveFormsModule for forms - declare the new ReactiveFormsModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { BtnComponent } from './components/btn/btn.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BtnComponent
+  ],
   imports: [
     CommonModule,
     RouterModule
+  ],
+  exports: [
+    BtnComponent,
+    ReactiveFormsModule // Required for forms
   ]
 })
 export class SharedModule { }
