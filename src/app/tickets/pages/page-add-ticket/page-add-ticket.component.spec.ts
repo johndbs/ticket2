57e011f9f2b61f94d9c0bf1cad212c13abc86440
johndbs <83272366+johndbs@users.noreply.github.com>
2024-06-20 10:41:02 +0200

    test: page-add-ticket.component

--- src/app/tickets/pages/page-add-ticket/page-add-ticket.component.spec.ts
+++ src/app/tickets/pages/page-add-ticket/page-add-ticket.component.spec.ts
@@ -1,23 +1,63 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PageAddTicketComponent } from './page-add-ticket.component';
+import { DebugElement } from '@angular/core';
+import { FormTicketComponent } from '../../components/form-ticket/form-ticket.component';
+import { TicketService } from '../../services/ticket.service';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
+import { TicketI } from '../../../core/interfaces/ticket-i';
 
 describe('PageAddTicketComponent', () => {
   let component: PageAddTicketComponent;
   let fixture: ComponentFixture<PageAddTicketComponent>;
+  let debugElement: DebugElement;
+
+  let ticketServiceMock: any;
+  let routerMock: any;
 
   beforeEach(async () => {
+
+    ticketServiceMock = {
+      createTicket: jest.fn().mockReturnValue(of({id: 1, title: 'Ticket 1', description: 'Description 1 ...'}))
+    };
+
+    routerMock = {
+      navigate: jest.fn()
+    };
+
+
     await TestBed.configureTestingModule({
-      declarations: [PageAddTicketComponent]
+      declarations: [PageAddTicketComponent, FormTicketComponent],
+      providers: [
+        {provide: TicketService, useValue: ticketServiceMock},
+        {provide: Router, useValue: routerMock}
+      ]
     })
     .compileComponents();
     
     fixture = TestBed.createComponent(PageAddTicketComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    debugElement = fixture.debugElement;
+    
   });
 
   it('should create', () => {
+    fixture.detectChanges(); // Triggers ngOnInit
     expect(component).toBeTruthy();
   });
+
+  it('should call createTicket', ()=>{
+
+    const ticket: TicketI = {id: 1, title: 'Title', description: 'Description ...'};
+    
+    fixture.detectChanges(); // Triggers ngOnInit
+
+    component.action(ticket);
+
+    expect(ticketServiceMock.createTicket).toHaveBeenCalledWith(ticket);
+    expect(routerMock.navigate).toHaveBeenCalledWith(['tickets']);
+  });
+
+
 });
